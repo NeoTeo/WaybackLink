b5fc005451aead027dd61e81d791ec76c445cd2d
NeoTEo <m@sartori.dk>
2021-06-15 15:43:49 +0200

    Initial Commit

---
+++ WaybackLink/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaybackLink
+//
+//  Created by teo on 15/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaybackLink/WaybackLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaybackLinkApp.swift
+//  WaybackLink
+//
+//  Created by teo on 15/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct WaybackLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
